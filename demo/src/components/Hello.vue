812cf04e625239586d3c13f0a68bd6a78b3d26a5
newbornhsir <765302230@qq.com>
2017-05-26 01:00:13 +0800

    route-to-tab

--- demo/src/components/Hello.vue
+++ demo/src/components/Hello.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="hello">
      <div class="form">
-       <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
-       <mt-field label="密码" placeholder="请输入密码" v-model="password"></mt-field>
-       <mt-checklist
+       <mt-field placeholder="请输入用户名" v-model="username"></mt-field>
+       <mt-field placeholder="请输入密码" v-model="password"></mt-field>
+       <!-- <mt-checklist
           v-model="selected"
+          id="transparent"
           :options="['记住密码']">
-        </mt-checklist>
+        </mt-checklist> -->
         <div class="submit-button">
           <mt-button type="primary" v-on:click="login">登录</mt-button>
         </div>
-        <transition name="fade">
-        	<router-link to="login/tab" v-if="isShow">Go to Foo</router-link>
-        </transition>
         
      </div>
   </div>
@@ -25,20 +23,19 @@ export default {
     return {
       username: '',
       password: '',
-      selected: [],
-      isShow: true
+      selected: []
     }
   },
   methods: {
     login: function () {
-      console.log(this.selected.length)
-      this.isShow = !this.isShow
+      // console.log(this.selected.length)
+      // this.isShow = !this.isShow
+      this.$router.push('/login/tab')
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>  
   .hello{
     display: flex;
@@ -47,13 +44,23 @@ export default {
     align-content:center; 
     width: 100%;
     height:100%;
-    background: silver;
+    background: url(../assets/bg.png);
+    background-size: 100% 100%;
   }
   .form{
   	width: 80%;
   }
+  .mint-field{
+    margin: 15px 0;
+    border-radius: 24px;
+  }
   .mint-checklist{
     text-align: right;
+    margin-top: -10px !important;
+  }
+  #transparent>.mint-cell{
+    /*这里不起作用，修改了mint-checklist下的默认样式*/
+    background-color: transparent !important;
   }
   .submit-button{
     text-align: center;
--- demo/src/components/tab/Tab.vue
+++ demo/src/components/tab/Tab.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="tab">
-  	<div>i am tab</div>
+    <mt-header title="home">
+	  <router-link to="/login" slot="left">
+	    <mt-button icon="back">返回</mt-button>
+	  </router-link>
+	  <mt-button icon="more" slot="right"></mt-button>
+	</mt-header>
+
+
+  	<mt-tabbar v-model="selected">
+	  <mt-tab-item id="外卖">
+	    <!-- <img slot="icon" src="../assets/100x100.png"> -->
+	    外卖
+	  </mt-tab-item>
+	  <mt-tab-item id="订单">
+	    <!-- <img slot="icon" src="../assets/100x100.png"> -->
+	    订单
+	  </mt-tab-item>
+	  <mt-tab-item id="发现">
+	    <!-- <img slot="icon" src="../assets/100x100.png"> -->
+	    发现
+	  </mt-tab-item>
+	  <mt-tab-item id="我的">
+	    <!-- <img slot="icon" src="../assets/100x100.png"> -->
+	    我的
+	  </mt-tab-item>
+	</mt-tabbar>
   </div>
 </template>
 <script>
@@ -18,12 +43,8 @@ export default {
 
 <style scoped>  
   .tab{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content:center; 
+    padding: 0 !important;
     width: 100%;
-    height:100%;
-    background: red;
+    height: 100%;
   }
 </style>
